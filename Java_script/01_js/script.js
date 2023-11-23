d3bb2c4b8fd393ab40558c4e32a8d9891078ab22
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2023-11-23 09:30:51 +0100

    end of lesson

---
+++ Java_script/01_js/script.js
@@ -0,0 +1,48 @@
+console.log("Hallo");
+
+
+console.log("Tolle Sache");
+
+
+
+
+// Variablen 
+
+let a = 7;
+
+let b = 8;
+
+let c = 10; 
+
+// Berechne folgendes und gib das Eregebnis über console.log aus 
+
+// a + b + c 
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+
+// (a + b) / c 
+let x = (a + b)/c;
+console.log("Das Ergebnis ist " + x);
+
+// c -a + b 
+let y = c - a + b;
+console.log("Das Ergebnis ist " + y);
+
+// a + b + c * 3 
+let t = a + b + c*3;
+console.log("7 + 8 + 10*3 = " + t);
+
+// (a + b + c)/4 
+
+ 
+
+// Ändere die Variablen auf folgende Werte 
+
+// a = 3 
+
+// b = a + 3 
+
+// c = c -3 
+
+// gib die neuen Werte der Variablen aus 
