de370f4a375739e7574cdda5a3c898a65355cc6d
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2023-11-30 08:17:05 +0100

    01_js_finished

--- Java_script/01_js/script.js
+++ Java_script/01_js/script.js
@@ -35,14 +35,21 @@ console.log("7 + 8 + 10*3 = " + t);
 
 // (a + b + c)/4 
 
- 
+ let Leonraider = (a + b + c)/4;
+ console.log("Das Ergebnis ist " + Leonraider);
 
 // Ändere die Variablen auf folgende Werte 
 
 // a = 3 
-
+a = 3;
 // b = a + 3 
-
+b = a+ 3;
 // c = c -3 
-
+c = c - 3;
 // gib die neuen Werte der Variablen aus 
+
+console.log(a);
+
+console.log(b);
+
+console.log(c);
