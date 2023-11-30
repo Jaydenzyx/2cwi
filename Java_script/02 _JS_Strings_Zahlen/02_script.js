d0227723d6179add6b5d3072ae0125523488d58a
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2023-11-30 08:36:15 +0100

    02 _JS_Strings_Zahlen_finished

---
+++ Java_script/02 _JS_Strings_Zahlen/02_script.js
@@ -0,0 +1,38 @@
+let a = 7;
+let b = 8;
+
+let ab = a + b;
+
+
+let c = 7.4;
+let d = 7.5;
+
+let cd = c + d;
+
+console.log(cd);
+
+
+//TEXTE STRINGS
+
+
+let an = "Andreas";
+
+console.log(an);
+
+console.log("Das Ergebnis von " + a + " + " + b + " = " + ab );
+
+
+
+let y = 17.4;
+let x = 3.9;
+
+let xy = y / x;
+
+console.log("The result of " + y + " divided by " + x + " is " + xy);
+
+
+let fn = "Jayden";
+
+let Nn = "Pribozic";
+
+console.log("Ich heiße " + fn + " " + Nn );
