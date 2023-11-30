3ec8e455b70ae5d5b520c4dfdf702f530c992c87
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2023-11-30 09:21:54 +0100

    04_Schleifen_finished

---
+++ Java_script/04_Schleifen/04_script.js
@@ -0,0 +1,60 @@
+console.log("test");
+
+//Schleife
+
+for (let count = 0; count < 10; count++) {
+    console.log(count + "Andreas");
+}
+
+console.log("Fertig");
+
+
+// Zähle von 7 bis 45 
+
+for (let i = 7; i < 46; i++) {
+    console.log(i);
+}
+
+console.log("Fertig");
+
+// Zähle von 9 bis 99 
+
+for (let a = 9; a < 100; a++) {
+    console.log(a);
+}
+
+console.log("Fertig");
+
+// Zähle rückwärts von 10 bis 0 
+
+for (let h = 10; h > -1; h--) {
+    console.log(h);
+}
+
+console.log("Fertig");
+
+// Zähle rückwärts von 1234 bis 1207 
+
+for (let h = 1234; h > 1206; h--) {
+    console.log(h);
+}
+
+console.log("Fertig");
+// Gib Deinen Namen genau 8 mal aus 
+
+for (let a = 0; a < 9; a++) {
+    console.log(a + " Jayden");
+}
+
+console.log("Fertig");
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle  von 10 auf 0 
+
+for (let count = 0; count < 11; count++) {
+    console.log(count);
+}
+
+console.log("Jayden");
+
+for (let y = 10; y > -1; y--) {
+    console.log(y);
+}
