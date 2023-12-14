16b280457ff1aaf4939555f19e2c71e76b6fce15
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2023-12-14 09:31:37 +0100

    end of lesson

---
+++ Java_script/09_AufgabeZ/script.js
@@ -0,0 +1,30 @@
+//Erstelle mit Hilfe einer Schleife und Modulo das folgende Ausgabe liefert (Rechteckbeispiel adaptieren):  
+//  X X X X  
+//  O O O O  
+//  X X X X  
+//  O O O O  
+
+for (let index = 0; index <4; index++) {
+    if (index%2 == 0){
+        console.log("xxxx");
+    }
+    else {
+        console.log("OOOO");
+    }
+} 
+
+
+
+//2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.)  
+
+ 
+
+//3. Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.:  
+
+//XX X XX X  
+
+//X    X X   X  
+
+//X            X 
+
+//X            X 
