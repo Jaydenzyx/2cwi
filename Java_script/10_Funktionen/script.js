38bb0f49f48632c4d3bd4564b4e8d55636ec581c
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-01-25 08:45:13 +0100

    10_funktionen_finished

--- Java_script/10_Funktionen/script.js
+++ Java_script/10_Funktionen/script.js
@@ -1,53 +1,43 @@
-function add(a,b) {
-    console.log(a+b);
+function add(a, b) {
+  console.log(a + b);
 }
 
-add(2,4);
+add(2, 4);
 
-function subtract(a,b) {
-    console.log(a-b);
-} 
-
-subtract(2,4);
-
-function mulitply(a,b) {
-    console.log(a*b);
+function subtract(a, b) {
+  console.log(a - b);
 }
 
-mulitply(2,4);
+subtract(2, 4);
 
-function supercalculation(a,b) {
-    console.log((a+b)/2 * a);
+function mulitply(a, b) {
+  console.log(a * b);
 }
 
-supercalculation(2,4);
+mulitply(2, 4);
+
+function supercalculation(a, b) {
+  console.log(((a + b) / 2) * a);
+}
+
+supercalculation(2, 4);
 
 function printEasterDate(J) {
-
-N = J-1900;
-A = N % 19;
-B = [(7*A+1)/19];
-M = (11*A+4-B) %29;
-Q = [N/4];
-W = (N+Q+31-M) % 7;;
-P = 25-M-W;
-
+  N = J - 1900;
+  A = N % 19;
+  B = [(7 * A + 1) / 19];
+  M = (11 * A + 4 - B) % 29;
+  Q = [N / 4];
+  W = (N + Q + 31 - M) % 7;
+  P = 25 - M - W;
 }
-
-
 
 printEasterDate(2023);
 
+if (P > 0) {
+  console.log("Der Oster Sonntag ist der " + Math.ceil(P) + " April");
+} else {
+  console.log("Der Obersonntag ist der " + Math.round(P + 31) + " März ");
+}
 
-    
-if (P>0) {
-    console.log("Der Oster Sonntag ist der " + Math.ceil(P) + " April");
-} 
-else { 
-   console.log("Der Obersonntag ist der " + Math.round(P + 31) + " März " ); 
-    
-} 
-
-
-
-//Diese Funktion bekommt das gewünschte Jahr und schreibt das Datum von Ostern. 
+//Diese Funktion bekommt das gewünschte Jahr und schreibt das Datum von Ostern.
