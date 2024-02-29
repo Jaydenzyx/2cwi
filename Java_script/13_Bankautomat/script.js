601883020ced839296784ba6cb5adca3bb4a510c
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-02-29 15:27:38 +0100

    13_Bankautomat_finished

--- Java_script/13_Bankautomat/script.js
+++ Java_script/13_Bankautomat/script.js
@@ -1,5 +1,6 @@
 import { log } from "console";
 import { read } from "fs";
+import { abort } from "process";
 import { createInterface } from "readline";
 
 const readline = createInterface({
@@ -31,22 +32,41 @@ while (!isfinished){
         console.log("Geben sie den Betrag ein den sie Einzahlen möchten");
         let Einzahlen = Number(await readLineAsync());
         if (Einzahlen>0) {
-            console.log("Ihre Einzahlung war Erfolgreich");
-      
             kontostand = kontostand + Einzahlen;
+            console.log("Ihre Einzahlung war Erfolgreich. Neuer Kontostand: " + kontostand);
         }
      
-        else if (Einzahlen<=0) {
-            console.log("Ihre Einzahlung war Ungültig");
+        else if (Einzahlen!=Number) {
+            console.log("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
         }
     }
        
     if (Selection == 2) {
-        console.log("Geben sie den Betrag ein den Sie abheben möchten");
-        let Abheben = Number(await readLineAsync());
-        
+      console.log("Geben Sie den Betrag ein, den Sie abheben möchten:");
+      let Abheben = Number(await readLineAsync());
+  
+      // Überprüfen, ob die Eingabe eine gültige Zahl ist
+      if (isNaN(Abheben)) {
+          console.log("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
+      } else {
+          if (Abheben > kontostand) {
+              console.log("Der Betrag konnte nicht abgehoben werden. Nicht genügend Guthaben vorhanden.");
+          } else {
+              kontostand -= Abheben;
+              console.log("Erfolgreich abgehoben. Neuer Kontostand: " + kontostand);
+          }
+      }
+  }
+  
 
+    if (Selection == 3)
+    console.log("Ihr Kontostand Beträgt:" + kontostand);
+
+
+    if (Selection == 4){
+      isfinished = true;
+      console.log("Tschüss");
+      process.exit(0);
     }
-
-
+    
 }
