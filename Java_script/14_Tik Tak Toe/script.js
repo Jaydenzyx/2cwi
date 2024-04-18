9a3f4538cbd9c79407ab50edfe9493c6edae7090
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-04-18 09:32:12 +0200

    end of lesson

--- Java_script/14_Tik Tak Toe/script.js
+++ Java_script/14_Tik Tak Toe/script.js
@@ -1,51 +1,16 @@
-import { log } from "console";
-import { read } from "fs";
-import { abort } from "process";
-import { createInterface } from "readline";
+const boxes = document.getElementsByClassName(box);
+let win = false;
+Currentplayer = "O";
 
-const readline = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+function printbox(x){ 
+if(boxes[x].innerHTML == "" && win != true) {
+  boxes[x].innerHTML = Currentplayer;
+    
+  if(Currentplayer == "X" && win != true){
+    Currentplayer = "O";
+  }
+else if(Currentplayer == "O" && win != true)
+  Currentplayer = "X";
+}
+}
 
-const readLineAsync = () => {
-  return new Promise((resolve) => {
-    readline.question("", (userRes) => {
-      resolve(userRes);
-    });
-  });
-};
-
-
-let field = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-];
-
-field [0][1] = 1;
-field [1][1] = 2;
-
-
-printField();
-
-
-function printField(){
-    for (let row = 0; row <field.length; row++) {
-       let actualrow = field[row];
-       let output = "";
-       for (let col = 0; col<actualrow.length;col++){
-        let sign =" ";
-        if (actualrow[col]==1){
-            sign = "X"
-        } else if (actualrow[col]== 2){
-            sign = "O"
-        }
-        
-        
-        output += sign + " | ";
-
-       } 
-       console.log (output);
-    }
-} 
