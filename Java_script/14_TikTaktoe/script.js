3e7838901e213fd346aeabffdd0b77a37b9bf242
Jaydenzyx <jayden.pribozic@student.htldornbirn.at>
2024-05-23 08:55:53 +0200

    Tiktaktoe_finished

--- Java_script/14_TikTaktoe/script.js
+++ Java_script/14_TikTaktoe/script.js
@@ -1,4 +1,3 @@
-import { userInfo } from "os";
 import { createInterface } from "readline";
 
 const readline = createInterface({
@@ -20,8 +19,6 @@ let field = [
   [0, 0, 0],
 ];
 
-printField();
-
 function printField() {
   for (let row = 0; row < field.length; row++) {
     let actualRow = field[row];
@@ -32,98 +29,73 @@ function printField() {
         sign = "X";
       } else if (actualRow[col] == 2) {
         sign = "O";
-
-        output += sign + " | ";
       }
-      console.log(output);
+      output += sign + " | ";
     }
+    console.log(output);
+    console.log("---------");
   }
 }
 
-let isfinished = false;
-let User1 = true;
-if (User1) {
-  console.log("Player 1´s turn");
-}
+let isFinished = false;
+let currentPlayer = 1;
 
-while (!isfinished) {
-  console.log("Select a Column");
-
+while (!isFinished) {
+  console.log(
+    "Player" + " " + [currentPlayer] + " " + "select a Column (0, 1, 2):"
+  );
   let inColumn = await readLineAsync();
 
   if (inColumn != 0 && inColumn != 1 && inColumn != 2) {
-    console.log("Invalid Column, please select a other Column");
+    console.log("Invalid Column, please select another Column");
+    continue;
   }
 
-  console.log("Select a Row");
+  console.log("Select a Row (0, 1, 2):");
+  let inRow = await readLineAsync();
 
-  let inrow = await readLineAsync();
-
-  if (inrow != 0 && inrow != 1 && inrow != 2) {
-    console.log("Invalid Row, please select a other row");
+  if (inRow != 0 && inRow != 1 && inRow != 2) {
+    console.log("Invalid Row, please select another Row");
+    continue;
   }
 
-  if (field[inrow][inColumn] != 0) {
-    console.log("Already in use, please select a other Column");
+  if (field[inRow][inColumn] != 0) {
+    console.log("Already in use, please select another cell");
+    continue;
   }
 
-  if (User1) {
-    console.log(field[inrow][inColumn] == 1);
+  field[inRow][inColumn] = currentPlayer;
+  printField();
 
-    User1 = false;
-    console.log("Player 2´s turn");
-  } else if (!User1) {
-    console.log(field[inrow][inColumn] == 2);
+  if (checkWin(currentPlayer)) {
+    console.log("Player" + " " + [currentPlayer] + " " + "Won!");
+    isFinished = true;
+    continue;
   }
 
-  User1 = true;
-  console.log("Player 1´s turn");
+  currentPlayer = currentPlayer === 1 ? 2 : 1;
+}
 
-  if (
-    field[0][0] == 1 &&
-    field[0][1] == 1 &&
-    field[0][2] == 1 &&
-    field[1][0] == 1 &&
-    field[1][1] == 1 &&
-    field[1][2] == 1 &&
-    field[2][0] == 1 &&
-    field[2][1] == 1 &&
-    field[2][2] == 1 &&
-    field[0][0] == 1 &&
-    field[1][0] == 1 &&
-    field[2][0] == 1 &&
-    field[0][1] == 1 &&
-    field[1][1] == 1 &&
-    field[2][1] == 1 &&
-    field[0][2] == 1 &&
-    field[1][2] == 1 &&
-    field[2][2] == 1
-  ) {
-    console.log("Player 1 Won!");
-    isfinished = true;
+function checkWin(player) {
+  for (let i = 0; i < 3; i++) {
+    if (
+      (field[i][0] == player &&
+        field[i][1] == player &&
+        field[i][2] == player) ||
+      (field[0][i] == player && field[1][i] == player && field[2][i] == player)
+    ) {
+      return true;
+    }
   }
 
   if (
-    field[0][0] == 2 &&
-    field[0][1] == 2 &&
-    field[0][2] == 2 &&
-    field[1][0] == 2 &&
-    field[1][1] == 2 &&
-    field[1][2] == 2 &&
-    field[2][0] == 2 &&
-    field[2][1] == 2 &&
-    field[2][2] == 2 &&
-    field[0][0] == 2 &&
-    field[1][0] == 2 &&
-    field[2][0] == 2 &&
-    field[0][1] == 2 &&
-    field[1][1] == 2 &&
-    field[2][1] == 2 &&
-    field[0][2] == 2 &&
-    field[1][2] == 2 &&
-    field[2][2] == 2
+    (field[0][0] == player && field[1][1] == player && field[2][2] == player) ||
+    (field[0][2] == player && field[1][1] == player && field[2][0] == player)
   ) {
-    console.log("Player 2 Won!");
-    isfinished = true;
+    return true;
+  } else {
+    return false;
   }
 }
+
+process.exit();
